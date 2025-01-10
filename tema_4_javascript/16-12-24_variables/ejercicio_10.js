e06474ae634630d6633b9c4a89c4818df37f1054
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-01-10 20:01:01 +0100

    <feat> add ejercicio 10

---
+++ tema_4_javascript/16-12-24_variables/ejercicio_10.js
@@ -0,0 +1,9 @@
+//Ejercicio 10: Conversión entre tipos básicos
+
+let numeroTexto = "123"
+let suma = numeroTexto + 10 //12310 --> se añade el 10 a la cadena de texto
+let resta = numeroTexto - 10 //113 --> realiza la operación al tratarse de una resta
+
+console.log(suma)
+console.log(resta)
+
