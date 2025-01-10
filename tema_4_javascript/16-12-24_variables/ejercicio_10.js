453bf068607f8e5fa22cf616887572179d282fe4
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-01-10 20:07:06 +0100

    <feat> add ejercicio 11

--- tema_4_javascript/16-12-24_variables/ejercicio_10.js
+++ tema_4_javascript/16-12-24_variables/ejercicio_10.js
@@ -2,7 +2,7 @@
 
 let numeroTexto = "123"
 let suma = numeroTexto + 10 //12310 --> se añade el 10 a la cadena de texto
-let resta = numeroTexto - 10 //113 --> realiza la operación al tratarse de una resta
+let resta = numeroTexto - 10 //113 --> realiza la operación al tratarse de una resta que no significa nada en cadenas de texto, js cambia el tipo de dato a número
 
 console.log(suma)
 console.log(resta)
---
+++ tema_4_javascript/16-12-24_variables/ejercicio_11.js
@@ -0,0 +1,11 @@
+//Ejercicio 12: Conversión de tipos
+
+let numeroComoCadena = "42"
+let numero = Number(numeroComoCadena)
+
+console.log(typeof(numero)) //number
+
+let boolean = false
+let booleanComoCadena = String(boolean)
+
+console.log(typeof(booleanComoCadena)) //string
