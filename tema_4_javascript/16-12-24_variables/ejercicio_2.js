65e8ccc517063fbe9b33187b2309351faf5ac4a1
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-01-10 19:12:03 +0100

    <feat> add ejercicio 2

---
+++ tema_4_javascript/16-12-24_variables/ejercicio_2.js
@@ -0,0 +1,10 @@
+//Ejercicio 2: Buenas prácticas en nombres de variables
+
+const nombreCompleto = "Sergio Lopez Ruiz"
+const colorFavorito = "Verde"
+let ciudadActual = "Cambo-les-Bains"
+
+//let 1cursoProgramacion = "FullStack Developer"
+
+// Al declarar una variable con un número al principio me lanza un error que dice que la declaracion de la variable no esta permitida de esta forma
+
