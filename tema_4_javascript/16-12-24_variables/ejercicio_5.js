1cb455c40c0ea8f96c76e3307c715824320a1fcd
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-01-10 19:35:35 +0100

    <feat> add ejercicio 5

---
+++ tema_4_javascript/16-12-24_variables/ejercicio_5.js
@@ -0,0 +1,16 @@
+//Ejercicio 5: Operadores aritméticos
+
+const x = 10
+const y = 3
+
+const SUMA = x + y //13
+const RESTA = x - y //7
+const MULTIPLICACION = x * y //30
+const DIVISION = x / y //3.33
+const MODULO = x % y //1
+
+console.log(SUMA)
+console.log(RESTA)
+console.log(MULTIPLICACION)
+console.log(DIVISION)
+console.log(MODULO)
