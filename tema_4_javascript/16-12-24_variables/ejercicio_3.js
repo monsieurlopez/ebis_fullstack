bc37da47b971b1650c9a3e85d0588f21a2d10b17
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-01-10 19:17:02 +0100

    <feat> add ejercicio 3

---
+++ tema_4_javascript/16-12-24_variables/ejercicio_3.js
@@ -0,0 +1,13 @@
+//Ejercicio 3: Tipos de datos básicos 
+
+let edad = 36
+let altura = 1.89
+const NOMBRE = "Sergio"
+let hijos = true
+let negacionista = null
+
+console.log(typeof(edad)) //number
+console.log(typeof(altura)) //number
+console.log(typeof(NOMBRE)) //string
+console.log(typeof(hijos)) //boolean
+console.log(typeof(negacionista)) //object
