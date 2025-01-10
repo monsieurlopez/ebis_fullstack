88da537e59a756dd8645284c45e184170396b305
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-01-10 19:41:02 +0100

    <feat> add ejercicio 7

---
+++ tema_4_javascript/16-12-24_variables/ejercicio_7.js
@@ -0,0 +1,9 @@
+//Ejercicio 7: Operadores de comparación
+
+const a = 5
+const b = "5"
+
+console.log(a == b) //true
+console.log(a === b) //false
+console.log(a != b) //false
+console.log(a !== b) //true
