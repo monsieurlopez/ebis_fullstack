b0321b774737801e88c36ba8f715e3422b6551e6
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-01-10 19:31:43 +0100

    <feat> add ejercicio 4

---
+++ tema_4_javascript/16-12-24_variables/ejercicio_4.js
@@ -0,0 +1,10 @@
+//Ejercicio 4: Diferencias entre undefined y null
+
+let sinValor;
+console.log(typeof sinValor); //undefined
+
+let vacio = null;
+console.log(typeof vacio); //object
+
+//La diferencia entre undefined y null es que undefined no tiene un valor definido y null tiene un valor definido nulo,
+//pero nulo es un valor que se podra reasignar más adelante
