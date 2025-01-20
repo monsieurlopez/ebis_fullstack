1246e49d9d9b9a18b779f74541884a4c7069b5f9
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-01-20 21:59:24 +0100

    <feat> add ejercicio 7

--- tema_4_javascript/13-01-25_objetos/ejer_7/app.js
+++ tema_4_javascript/13-01-25_objetos/ejer_7/app.js
@@ -4,9 +4,7 @@ const PLANETA3 = window.prompt("Introduce el último nombre de un planeta de nue
 
 const PLANETAS_USUARIO = [PLANETA1, PLANETA2, PLANETA3]
 
-if (PLANETA1 !== PLANETA2 && PLANETA1 !== PLANETA3 && PLANETA2 !== PLANETA3) {
-    console.log("Todos los planetas son diferentes")
-} else {
+if (PLANETA1 === PLANETA2 && PLANETA1 === PLANETA3 && PLANETA2 === PLANETA3) {
     window.alert("Al menos dos planetas son iguales. Introduce tres diferentes por favor.")
     window.location.reload()
 }
@@ -24,8 +22,6 @@ for (let i = 0; i < PLANETAS_USUARIO.length; i++) {
     }
 }
 
-console.log(noExisten)
-
 if (noExisten.length > 0) {
     for (i = 0; i < noExisten.length; i++) {
         window.alert(`El planeta ${noExisten[i]} no existe en nuestro sistema solar.`)
@@ -33,3 +29,22 @@ if (noExisten.length > 0) {
     window.alert("Escribe de nuevo tres planetas que existan.")
     window.location.reload()
 }
+
+let nombre, imagen, color, temperatura
+let numero = 1
+
+for (planeta of PLANETAS_USUARIO) {
+    nombre = sistemaSolar[planeta]["nombre"]
+    imagen = sistemaSolar[planeta]["imagen"]
+    color = sistemaSolar[planeta]["color"]
+    temperatura = sistemaSolar[planeta]["temperatura"]
+
+    document.getElementById(`foto_planeta${numero}`).src = imagen
+    document.getElementById(`nombre_planeta${numero}`).innerHTML = `Nombre: ${nombre}`
+    document.getElementById(`color_planeta${numero}`).innerHTML = `Color: ${color}`
+    document.getElementById(`temperatura_planeta${numero}`).innerHTML = `Temperatura: ${temperatura}`
+
+    numero++
+}
+
+
