e9ba6f09b7cbce0dc52e59c51bd2ff1a49b1ba9c
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-01-20 16:08:30 +0100

    <fix> update ejercicio 3

--- tema_4_javascript/13-01-25_objetos/ejer_3/app.js
+++ tema_4_javascript/13-01-25_objetos/ejer_3/app.js
@@ -1,8 +1,15 @@
 const NUM = Number(window.prompt("Escribe un número:"))
 
 function insertNumero (NUM) {
+    let color = "yellow"
     const INSERT_NUM = document.getElementById("numero_insertado")
     INSERT_NUM.innerHTML = NUM
+    if (NUM < 100) {
+        color = "green"
+    } else if (NUM > 200) {
+        color = "red"
+    }
+    INSERT_NUM.style.color = color
 }
 
 insertNumero(NUM)
