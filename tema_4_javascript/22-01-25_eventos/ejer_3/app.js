4b4db1eb109b17ac163adbfc4a77b9a880bd28df
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-01-26 11:47:53 +0100

    <fix> create function update shooping cart

--- tema_4_javascript/22-01-25_eventos/ejer_3/app.js
+++ tema_4_javascript/22-01-25_eventos/ejer_3/app.js
@@ -32,9 +32,48 @@ for (const PRODUCTO of PRODUCTOS_EN_VENTA) {
         <img src="${PRODUCTO.img}" alt="${PRODUCTO.name} class="img_producto">
         <p class="price"> <strong>Precio:</strong> ${PRODUCTO.price}€ </p>
         <div>
-            <button id="btn_add_${PRODUCTO.name}" class="btn"> ➕ </button>
-            <button id="btn_subtract_${PRODUCTO.name}" class="btn"> ➖ </button>
+            <button id="btn_add_${PRODUCTO.name}" class="btn" onclick="actualizarCesta('add','${(PRODUCTO.name).toLowerCase()}','${PRODUCTO.price}')"> ➕ </button>
+            <button id="btn_subtract_${PRODUCTO.name}" class="btn" onclick="actualizarCesta('subtract','${(PRODUCTO.name).toLowerCase()}','${PRODUCTO.price}')"> ➖ </button>
         </div>
     `
     BOX_TOOLS.appendChild(DIV_PRODUCTO);
 }
+
+let cestaCompra = [
+    {
+        producto: "brocha",
+        cantidad: 0,
+        total: 0
+    },
+    {
+        producto: "llave",
+        cantidad: 0,
+        total: 0
+    },
+    {
+        producto: "martillo",
+        cantidad: 0,
+        total: 0
+    },
+    {
+        producto: "tronco",
+        cantidad: 0,
+        total: 0
+    },
+]
+
+
+function actualizarCesta(accion, producto, precio) {
+    for (let i = 0; i < cestaCompra.length; i++) {
+        if (cestaCompra[i].producto === producto) {
+            if (accion === "add") {
+                cestaCompra[i].cantidad++
+            } else {
+                cestaCompra[i].cantidad--
+            }
+            cestaCompra[i].total = (cestaCompra[i].cantidad) * precio
+        }
+    }
+    console.log(cestaCompra)
+}
+
