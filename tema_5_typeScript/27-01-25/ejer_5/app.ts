3d2f3ee0d2d38c533e5cff8005a002b969468374
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-02-06 00:41:32 +0100

    <fix> error sintax exercise 5

--- tema_5_typeScript/27-01-25/ejer_5/app.ts
+++ tema_5_typeScript/27-01-25/ejer_5/app.ts
@@ -1,7 +1,7 @@
 //* Declaration function
 function addAllThisNumbers(num1 : number, num2 : number, ...restOfNumbers : number[]): void {
     if ([num1, num2, ...restOfNumbers].every(param => typeof param === 'number')) {
-        console.log(num1 + num2 + restOfNumbers.reduce((total, num) => total + num, 0);
+        console.log(num1 + num2 + restOfNumbers.reduce((total, num) => total + num, 0));
     } else {
         console.log("Todos los parámetros deben ser números.");
     }
