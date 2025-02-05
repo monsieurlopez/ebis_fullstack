c28629e0450836e4843ad7e10af5ff9366077519
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-02-06 00:30:20 +0100

    <fix> update exercise 1

--- tema_5_typeScript/27-01-25/ejer_1/app.ts
+++ tema_5_typeScript/27-01-25/ejer_1/app.ts
@@ -1,8 +1,8 @@
-const SALUDO = "Hola mundo!"
-console.log("Saludo :" + " " + SALUDO)
+const SALUDO: string = "Hola mundo!";
+console.log("Saludo :" + " " + SALUDO);
 
-let nuevoSaludo = "Hola, este es el nuevo saludo"
-console.log(nuevoSaludo)
+let nuevoSaludo: string = "Hola, este es el nuevo saludo";
+console.log(nuevoSaludo);
 
-nuevoSaludo = "Hola, el mensaje ha cambiado"
-console.log(nuevoSaludo)
+nuevoSaludo = "Hola, el mensaje ha cambiado";
+console.log(nuevoSaludo);
