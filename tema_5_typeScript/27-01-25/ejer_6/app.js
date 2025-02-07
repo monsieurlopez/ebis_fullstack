8da79395543644e2b635806a3fb804d8e21f271c
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-02-07 08:34:41 +0100

    <fix> update and modif exercise 6

--- tema_5_typeScript/27-01-25/ejer_6/app.js
+++ tema_5_typeScript/27-01-25/ejer_6/app.js
@@ -1,16 +1,21 @@
 function printHumanReadableDate(fecha) {
+    var locale = "ES";
+    var options = { weekday: 'long', day: 'numeric', month: 'long' };
     var fechaFormateada;
     if (fecha instanceof Date) {
         fechaFormateada = fecha;
     }
     else {
         fechaFormateada = new Date(fecha);
+        if (isNaN(fechaFormateada.getTime())) {
+            console.log("Formato de fecha no valido.");
+            return;
+        }
     }
-    var opciones = { weekday: 'long', day: 'numeric', month: 'long' };
-    var FECHA_DESEADA = fechaFormateada.toLocaleDateString('es-ES', opciones);
+    var FECHA_DESEADA = new Intl.DateTimeFormat(locale, options).format(fechaFormateada);
     console.log(FECHA_DESEADA);
 }
 printHumanReadableDate(new Date()); // viernes, 7 de febrero
 printHumanReadableDate("2025-01-15T11:56:03.673Z"); //miércoles, 15 de enero
-printHumanReadableDate("2025-01-15T11"); // Invalid date
-printHumanReadableDate("Hola"); // Invalid date
+printHumanReadableDate("2025-01-15T11"); // Formato de fecha no valido.
+printHumanReadableDate("Hola"); // Formato de fecha no valido.
--- tema_5_typeScript/27-01-25/ejer_6/app.ts
+++ tema_5_typeScript/27-01-25/ejer_6/app.ts
@@ -1,20 +1,26 @@
 type DateInput = string | Date
 
 function printHumanReadableDate (fecha : DateInput) : void {
+    let locale : string = "ES"
+    let options: object = { weekday: 'long', day: 'numeric', month: 'long' };
+
     let fechaFormateada : Date;
     if (fecha instanceof Date) {
         fechaFormateada = fecha
     } else {
         fechaFormateada = new Date(fecha)
+        if (isNaN(fechaFormateada.getTime())) {
+            console.log("Formato de fecha no valido.");
+            return;
+        }
     }
 
-    let opciones: object = { weekday: 'long', day: 'numeric', month: 'long' };
-    const FECHA_DESEADA = fechaFormateada.toLocaleDateString('es-ES', opciones);
-    console.log(FECHA_DESEADA);
-
+    const FECHA_DESEADA = new Intl.DateTimeFormat(locale, options).format(fechaFormateada);
+    console.log(FECHA_DESEADA)
 }
 
 printHumanReadableDate(new Date()) // viernes, 7 de febrero
 printHumanReadableDate("2025-01-15T11:56:03.673Z") //miércoles, 15 de enero
-printHumanReadableDate("2025-01-15T11") // Invalid date
-printHumanReadableDate("Hola") // Invalid date
+printHumanReadableDate("2025-01-15T11") // Formato de fecha no valido.
+printHumanReadableDate("Hola") // Formato de fecha no valido.
+
