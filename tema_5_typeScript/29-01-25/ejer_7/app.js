c1ba5141326998e7fd69ffad49a6f499c80e7a3e
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-02-07 10:53:49 +0100

    <feat> start exercise 7

--- tema_5_typeScript/29-01-25/ejer_7/app.js
+++ tema_5_typeScript/29-01-25/ejer_7/app.js
@@ -1,1 +1,17 @@
-console.log("Hola");
+var button = document.getElementById("btn_submit");
+function submit() {
+    var input = document.getElementById("input");
+    if (input) {
+        var INPUT_VALUE = input.value.trim();
+        /*if (INPUT_VALUE === "" || INPUT_VALUE ===) {
+            console.error("Invalid input value")
+            input.value = ""
+        }*/
+        console.log(INPUT_VALUE);
+        input.value = "";
+    }
+    else {
+        console.error("No se encontró el input");
+    }
+}
+button === null || button === void 0 ? void 0 : button.addEventListener("click", submit);
--- tema_5_typeScript/29-01-25/ejer_7/app.ts
+++ tema_5_typeScript/29-01-25/ejer_7/app.ts
@@ -1,1 +1,20 @@
-console.log("Hola")
+let button = document.getElementById("btn_submit") as HTMLButtonElement | null;
+
+function submit(): void {
+    let input = document.getElementById("input") as HTMLInputElement | null;
+    if (input) {
+        const INPUT_VALUE : string = input.value.trim()
+        /*if (INPUT_VALUE === "" || INPUT_VALUE ===) {
+            console.error("Invalid input value")
+            input.value = ""
+        }*/
+        console.log(INPUT_VALUE);
+        input.value = ""
+    } else {
+        console.error("No se encontró el input");
+    }
+}
+
+button?.addEventListener("click", submit);
+
+
