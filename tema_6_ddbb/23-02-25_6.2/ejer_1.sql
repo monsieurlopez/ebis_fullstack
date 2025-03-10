763de00d8cabf5647553945be6fc0752fc1d4536
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-03-11 00:48:55 +0100

    <feat> add ejer 1

---
+++ tema_6_ddbb/23-02-25_6.2/ejer_1.sql
@@ -0,0 +1,21 @@
+/** Tema 1: Índices **/
+/* Ejercicio 1: Creación de Índices */
+
+--1. Dado el siguiente esquema de tabla:--
+
+CREATE TABLE productos (
+    id INT PRIMARY KEY,
+    nombre VARCHAR(100),
+    precio DECIMAL(10, 2),
+    categoria_id INT
+);
+
+-- Crea un índice simple sobre la columna nombre --
+CREATE INDEX idx_nombre ON productos (nombre);
+
+-- Crea un índice compuesto sobre las columnas precio y categoria_id --
+CREATE INDEX idx_precio_categoria ON productos (precio, categoria_id);
+
+-- Verifica los índices creados --
+SHOW INDEXES FROM productos;
+
