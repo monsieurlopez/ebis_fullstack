974d05f431ef840eb68d96140397617d9329567a
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-03-11 00:39:12 +0100

    <feat> start ejer 5

---
+++ tema_6_ddbb/23-02-25_ER_lenguajes/ejer_5.sql
@@ -0,0 +1,4 @@
+/** Transacciones: **/
+/* Ejercicio 1: Simular un proceso de compra en línea donde se realizan varias operaciones como la actualización
+del inventario y la creación de un registro de pedido. Usar transacciones para asegurar consistencia */
+
