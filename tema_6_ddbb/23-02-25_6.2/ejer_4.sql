d686cb00ea4c77338ff8f6636ada37f296c7f619
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-03-13 17:30:39 +0100

    <feat> add ejer 4

--- tema_6_ddbb/23-02-25_6.2/ejer_4.sql
+++ tema_6_ddbb/23-02-25_6.2/ejer_4.sql
@@ -20,7 +20,7 @@ DELIMITER $$
         IN p_email VARCHAR(100)
     )
     BEGIN
-        INSERT INTO clientes (nombre, email) 
+        INSERT INTO clientes (nombre, email)
         VALUES (p_nombre, p_email);
     END$$
 
@@ -30,3 +30,28 @@ DELIMITER ;
 -- Llama al procedimiento para insertar un nuevo cliente --
 call insertar_cliente ('Sergio Lopez', 'sergio@gmail.com' );
 call insertar_cliente ('Miguel Lopez', 'miguel@gmail.com' );
+
+/* Ejercicio 2: Procedimiento con Parámetros de Salida */
+
+-- Crea un procedimiento que reciba un id de cliente y devuelva su nombre y email como parámetros de salida --+
+
+DELIMITER $$
+    CREATE PROCEDURE obtener_nombre_email_cliente (
+        IN p_id_cliente INT,
+        OUT p_nombre VARCHAR(100),
+        OUT p_email VARCHAR(100)
+    )
+    BEGIN
+        SELECT nombre, email
+        INTO p_nombre, p_email
+        FROM clientes
+        WHERE id_cliente = p_id_cliente;
+    END$$
+
+DELIMITER ;
+
+
+--Llama al procedimiento para recuperar la información de un cliente --
+
+CALL obtener_nombre_email_cliente(1, @nombre, @email);
+SELECT @nombre, @email;
