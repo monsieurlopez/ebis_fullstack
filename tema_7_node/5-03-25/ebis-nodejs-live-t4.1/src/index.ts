555d49743649ee6f7bbc4c0538ab00feedc6d0f9
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-04-15 01:26:42 +0200

    <feat> add exercise Mongodb Atlas and Postman

--- tema_7_node/5-03-25/ebis-nodejs-live-t4.1/src/index.ts
+++ tema_7_node/5-03-25/ebis-nodejs-live-t4.1/src/index.ts
@@ -8,7 +8,9 @@ import {
   UpdateResult,
   WithId,
 } from "mongodb";
-import { client, dbName } from "../src/db/mongodb";
+import dotenv from "dotenv";
+import { client } from "../src/db/mongodb";
+dotenv.config();
 
 const app: Express = express();
 const port: number | string = process.env.PORT ?? 3000;
@@ -17,80 +19,51 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const dbName = process.env.DB_NAME;
+
 app.get("/", (req: Request, res: Response) => {
   res.redirect("/tasks");
 });
 
+// TODO: Allow filtering by name/description/isDone/comma-separated ids
 app.get("/tasks", async (req: Request, res: Response) => {
-  // TODO: Allow filtering by name/description/isDone/comma-separated ids
-  // as we did with the file-based tasks
   try {
-    const taskDocuments: WithId<Document>[] = await client
+    const tasksDocuments = await client
       .db(dbName)
       .collection("tasks")
       .find()
       .toArray();
-    const tasks: Task[] = taskDocuments.map(
-      (taskDocument: WithId<Document>) => {
-        const { _id, name, done, description } = taskDocument;
-        return {
-          id: _id.toString(),
-          name: String(name),
-          description: String(description),
-          isDone: !!done,
-        };
-      }
-    );
-
-    res.send(tasks);
+    res.send(tasksDocuments);
   } catch (error) {
     console.error("Error reading tasks from DB:", error);
-    res.status(500).send([]);
   }
 });
 
 app.get("/tasks/:id", async (req: Request, res: Response) => {
   try {
-    const taskDocument: WithId<Document> | null = await client
-      .db(dbName)
-      .collection("tasks")
-      // What happens if the id is not a valid ObjectId? (e.g. too short)
-      .findOne({ _id: new ObjectId(req.params.id) });
-
-    if (!taskDocument) {
-      res.status(404).send("Task not found");
-    } else {
-      const { _id, name, done, description } = taskDocument;
-      res.send({ id: _id.toString(), name, done, description });
-    }
+    
   } catch (error) {
-    console.error("Error reading task from DB:", error);
-    res.status(500).send("Error reading task");
   }
 });
 
 app.post("/tasks", async (req: Request, res: Response) => {
-  const { name, description, done } = req.body;
-  const newTask: Task = { name, description, isDone: done };
   // TODO: Error on non-unique names and bad params as we did on the file ones
   // - Name is required
   // - Description, if present, has to be a string
   // - isDone (received as done), if not present, defaults to false. If present, it has to be a boolean
-
   try {
-    const result: InsertOneResult<Document> = await client
-      .db(dbName)
-      .collection("tasks")
-      .insertOne(newTask);
+    const { name, description, done } = req.body;
 
-    // Any of these can be done, or result can be ignored
-    // newTask.id = result.insertedId.toString();
-    // newTask.id = delete newTask._id;
-    console.log("Task added", newTask);
-    res.send(201);
+    const newTask : Task = {
+      name,
+      description,
+      isDone: typeof done === "boolean" ? done : false
+    };
+
+    await client.db(dbName).collection("tasks").insertOne(newTask);
+    res.sendStatus(201);
   } catch (error) {
-    console.error("Error adding task to DB:", error);
-    res.send(500);
+    console.error("Error creating task in DB:", error);
   }
 });
 
