818da289218df364000862e4626ebc28a8dd5144
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-07-01 19:21:13 +0200

    <feat> install tailwind and flowbite

--- tema_9_react/ebis-app/src/main.tsx
+++ tema_9_react/ebis-app/src/main.tsx
@@ -1,12 +1,13 @@
+import 'flowbite';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 //import App from './App.tsx';
-import { Navbar } from './components/Navbar.tsx';
+import { Header } from './components/Header.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Navbar />
+    <Header />
     {/*<App /> */}
   </StrictMode>
 );
