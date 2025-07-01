d91975a4afe4fd1713a3b7262259c9833cc67353
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-07-01 18:35:25 +0200

    <feat> create component NavBar

---
+++ tema_9_react/ebis-app/src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+export const Navbar = () => {
+
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    );
+};
+
